a2c787fdb6f3b085e592f158b58fcc8070601fe0
How-Bout-No <10456231+How-Bout-No@users.noreply.github.com>
2018-07-01 23:16:02 -0400

    Update index.js

--- docs/chatroom/index.js
+++ docs/chatroom/index.js
@@ -6,84 +6,6 @@ function log(text) {
   $('log').value += text + '\n';
 }
 
-function xhrWithAuth(method, url, interactive, callback) {
-  var access_token;
-
-  var retry = true;
-
-  getToken();
-
-  function getToken() {
-    chrome.identity.getAuthToken({ interactive: interactive }, function(token) {
-      if (chrome.runtime.lastError) {
-        callback(chrome.runtime.lastError);
-        return;
-      }
-
-      access_token = token;
-      requestStart();
-    });
-  }
-
-  function requestStart() {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + access_token);
-    xhr.onload = requestComplete;
-    xhr.send();
-  }
-
-  function requestComplete() {
-    if (this.status == 401 && retry) {
-      retry = false;
-      chrome.identity.removeCachedAuthToken({ token: access_token },
-                                            getToken);
-    } else {
-      callback(null, this.status, this.response);
-    }
-  }
-}
-
-var person;
-function onUserInfoFetched(error, status, response) {
-  if (!error && status == 200) {
-    user_info = JSON.parse(response);
-    person = user_info.name.givenName;
-  }
-}
-
-chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-  xhrWithAuth('GET', 'https://www.googleapis.com/plus/v1/people/me', true, onUserInfoFetched);
-});
-
-function revokeToken() {
-  user_info_div.innerHTML="";
-  chrome.identity.getAuthToken({ 'interactive': false },
-    function(current_token) {
-      if (!chrome.runtime.lastError) {
-
-        // @corecode_begin removeAndRevokeAuthToken
-        // @corecode_begin removeCachedAuthToken
-        // Remove the local cached token
-        chrome.identity.removeCachedAuthToken({ token: current_token },
-          function() {});
-        // @corecode_end removeCachedAuthToken
-
-        // Make a request to revoke token in the server
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://accounts.google.com/o/oauth2/revoke?token=' +
-                 current_token);
-        xhr.send();
-        // @corecode_end removeAndRevokeAuthToken
-
-        // Update the user interface accordingly
-        changeState(STATE_START);
-        sampleSupport.log('Token revoked and removed from cache. '+
-          'Check chrome://identity-internals to confirm.');
-      }
-  });
-}
-
 var port = 9999;
 var isServer = false;
 if (http.Server && http.WebSocketServer) {
@@ -114,7 +36,7 @@ if (http.Server && http.WebSocketServer) {
     // connected sockets.
     socket.addEventListener('message', function(e) {
       for (var i = 0; i < connectedSockets.length; i++)
-        connectedSockets[i].send(person + ': ' + e.data);
+        connectedSockets[i].send(e.data);
     });
 
     // When a socket is closed, remove it from the list of connected sockets.
